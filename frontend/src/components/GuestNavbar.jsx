ce037c6bc1c23fcff500732a7e3c08a402b5de7c
FioMartizano <144452744+FioMartizano@users.noreply.github.com>
2026-06-25 19:03:31 +0800

    pagana

---
+++ frontend/src/components/GuestNavbar.jsx
@@ -0,0 +1,9 @@
+function GuestNavbar() {
+    return (
+        <nav className="bg-orange-600 text-white p-4">
+            <h1 className="text-4xl font-bold">Test</h1>
+        </nav>
+    );
+}
+
+export default GuestNavbar;
---
+++ frontend/src/pages/GuestHomePage.jsx
@@ -0,0 +1,10 @@
+function GuestHomePage () {
+    return (
+        <main classNam="min-h-screen p-8">
+            <h2 className="text-3xl font-bold">Subdiverse</h2>
+        
+        </main>
+    );
+}
+
+export default GuestHomePage;

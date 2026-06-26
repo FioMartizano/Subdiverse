4c8d66a2a53f6e604b8f4a6586f48c29dccf5b7e
ACgumarang <criseldagumarang@gmail.com>
2026-06-27 00:27:38 +0800

    aboutus(notyetdone)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,23 @@
-/*COMPONENTS*/
+
+import { Routes, Route } from 'react-router-dom';  
+
 import Footer from './components/Footer';
 import GuestNavbar from './components/GuestNavbar';
-
 /*GUEST PAGE*/
 import GuestHomePage from './pages/Guest/GuestHomePage';
-
+import AboutUs from './pages/Guest/AboutUs';
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <GuestNavbar />
 
+
       <main className="flex-grow">
-        <GuestHomePage />
+        <Routes>
+          <Route path="/" element={<GuestHomePage />} />
+          <Route path="/about" element={<AboutUs />} />
+        </Routes>
       </main>
 
       <Footer />

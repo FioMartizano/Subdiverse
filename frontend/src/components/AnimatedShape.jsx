a08e33abe0a355f8d9808a584d227436d0168862
FioMartizano <144452744+FioMartizano@users.noreply.github.com>
2026-06-28 22:35:27 +0800

    Contact Us + Animation Framer

--- frontend/src/components/AnimatedShape.jsx
+++ frontend/src/components/AnimatedShape.jsx
@@ -1,13 +1,19 @@
 import { useScroll, useTransform, motion } from "framer-motion";
+import { useRef } from "react";
 
-function AnimatedShape({ className = "", range = [0, -80]}) {
-    const { scrollYProgress } = useScroll();
+function AnimatedShape({ className = "", range = [0, -500] }) {
+    const ref = useRef(null);
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["start end", "end start"],  
+    });
     const y = useTransform(scrollYProgress, [0, 1], range);
 
     return (
         <motion.div
-        style={{ y }}
-        className={`absolute rounded-[50%] bg-secondary z-0 ${className}`}
+            ref={ref}
+            style={{ y }}
+            className={`absolute rounded-[50%] bg-secondary z-0 ${className}`}
         />
     );
 }

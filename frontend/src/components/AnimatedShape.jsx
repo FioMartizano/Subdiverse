d4afb379ed061eda6463ca6f6986e6aa2062feeb
FioMartizano <144452744+FioMartizano@users.noreply.github.com>
2026-06-29 19:37:39 +0800

    AnimatedShape modified

--- frontend/src/components/AnimatedShape.jsx
+++ frontend/src/components/AnimatedShape.jsx
@@ -1,21 +1,32 @@
 import { useScroll, useTransform, motion } from "framer-motion";
 import { useRef } from "react";
 
-function AnimatedShape({ className = "", range = [0, -500] }) {
-    const ref = useRef(null);
-    const { scrollYProgress } = useScroll({
-        target: ref,
-        offset: ["start end", "end start"],  
-    });
-    const y = useTransform(scrollYProgress, [0, 1], range);
+function AnimatedShape({
+  className = "",
+  xRange = [0, 80],
+  yRange = [0, -500],
+  rotateRange = [0, 0],
+  scaleRange = [1, 1],
+}) {
+  const ref = useRef(null);
 
-    return (
-        <motion.div
-            ref={ref}
-            style={{ y }}
-            className={`absolute rounded-[50%] bg-secondary z-0 ${className}`}
-        />
-    );
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "end start"],
+  });
+
+  const x = useTransform(scrollYProgress, [0, 1], xRange);
+  const y = useTransform(scrollYProgress, [0, 1], yRange);
+  const rotate = useTransform(scrollYProgress, [0, 1], rotateRange);
+  const scale = useTransform(scrollYProgress, [0, 1], scaleRange);
+
+  return (
+    <motion.div
+      ref={ref}
+      style={{ x, y, rotate, scale }}
+      className={`absolute rounded-[50%] bg-secondary z-0 ${className}`}
+    />
+  );
 }
 
 export default AnimatedShape;
